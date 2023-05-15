60f2beb16b5af7cc9c12818a5becf3dd55592c1f
root <root@ip-172-31-17-129.ap-southeast-2.compute.internal>
2023-05-15 05:16:41 +0000

    Changes

---
+++ commit.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# Script for Git commit - By - MK
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+reset=$(tput sgr0)
+
+echo "${magenta} Git status${reset}"
+git status 
+echo "--------------------------"
+
+echo "${cyan} Git add all${reset}"
+git add . -v
+echo "--------------------------"
+echo "${green} Git commit${reset}"
+git commit -m "Changes" -v
+echo "--------------------------"
+echo "${yellow} Git status${reset}"
+git status 
+echo "--------------------------"
+echo "${blue} Git push started${reset}"
+git push -u origin master
+echo "--------------------------"
+echo "${magenta}Successfully updated${reset}"
